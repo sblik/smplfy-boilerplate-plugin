b1ea92ccae6ab23fd1cb2fe56203c27573146459
TomCom1234 <tomcom12344@gmail.com>
2024-09-17 16:45:59 +0100

    USe correct constant name

--- includes/enqueue_scripts.php
+++ includes/enqueue_scripts.php
@@ -11,8 +11,8 @@ function bs_enqueue_boilerplate_frontend_scripts() {
 	global $post;
 	$current_user = wp_get_current_user();
 
-	wp_register_script( 'bs-demo-frontend-script', BS_NAME_PLUGIN_URL . '/includes/js/frontend.js', array( 'jquery' ), null, true );
-	wp_register_style( 'bs-demo-frontend-styles', BS_NAME_PLUGIN_URL . '/includes/css/frontend.css' );
+	wp_register_script( 'bs-demo-frontend-script', SMPLFY_NAME_PLUGIN_URL . '/includes/js/frontend.js', array( 'jquery' ), null, true );
+	wp_register_style( 'bs-demo-frontend-styles', SMPLFY_NAME_PLUGIN_URL . '/includes/css/frontend.css' );
 
 	wp_enqueue_script( 'bs-demo-frontend-script' );
 	wp_enqueue_style( 'bs-demo-frontend-styles' );
