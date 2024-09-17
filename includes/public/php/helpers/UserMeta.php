d2f666a05bdf11cb9ea915e3431e99a1efaf483f
TomCom1234 <tomcom12344@gmail.com>
2024-09-17 16:04:05 +0100

    Add user meto class

---
+++ includes/public/php/helpers/UserMeta.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace SMPLFY\boilerplate;
+use SmplfyCore\SMPLFY_Log;
+
+class UserMeta {
+	public static $phoneMetaRow = 'phone';
+	public static $superUniqueMeta = 'super_unique_meta';
+
+	/**
+	 * @param $userID
+	 * @param $metaKey
+	 * @param $metaValue
+	 *
+	 * @return void
+	 */
+	public static function store_user_meta( $userID, $metaKey, $metaValue ) {
+		delete_user_meta( $userID, $metaKey );
+		add_user_meta( $userID, $metaKey, $metaValue );
+		self::validate_add_user_meta( $userID, $metaKey, $metaValue );
+	}
+
+	/**
+	 * @param $userID
+	 * @param $metaKey
+	 *
+	 * @return mixed
+	 */
+	public static function retrieve_user_meta( $userID, $metaKey ) {
+		return get_user_meta( $userID, $metaKey, true );
+	}
+
+	/**
+	 * @param $userID
+	 * @param $metaKey
+	 * @param $metaValue
+	 *
+	 * @return void
+	 */
+	private static function validate_add_user_meta( $userID, $metaKey, $metaValue ) {
+		$returnedMetaValue = get_user_meta( $userID, $metaKey, $metaValue );
+		if ( strval( $returnedMetaValue ) !== strval( $metaValue ) ) {
+			SMPLFY_Log::error( "META VALUE NOT STORED! User ID: $userID Row that wasn't created: $metaKey" );
+		}
+	}
+}
