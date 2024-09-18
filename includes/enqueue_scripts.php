d22c68d6bc672d063886845641110c33175110a7
TomCom1234 <tomcom12344@gmail.com>
2024-09-18 09:14:34 +0100

    Remove bs prefix and replace some with SMPLFY

--- includes/enqueue_scripts.php
+++ includes/enqueue_scripts.php
@@ -1,21 +1,21 @@
 <?php
 /*
  *
- * * Enqueue scripts on Website
+ * * Enqueue scripts on Wesmplfyite
  *
  *  */
 
 namespace SMPLFY\boilerplate;
-function bs_enqueue_boilerplate_frontend_scripts() {
+function enqueue_boilerplate_frontend_scripts() {
 	global $current_user;
 	global $post;
 	$current_user = wp_get_current_user();
 
-	wp_register_script( 'bs-demo-frontend-script', SMPLFY_NAME_PLUGIN_URL . '/includes/js/frontend.js', array( 'jquery' ), null, true );
-	wp_register_style( 'bs-demo-frontend-styles', SMPLFY_NAME_PLUGIN_URL . '/includes/css/frontend.css' );
+	wp_register_script( 'smplfy-demo-frontend-script', SMPLFY_NAME_PLUGIN_URL . '/includes/js/frontend.js', array( 'jquery' ), null, true );
+	wp_register_style( 'smplfy-demo-frontend-styles', SMPLFY_NAME_PLUGIN_URL . '/includes/css/frontend.css' );
 
-	wp_enqueue_script( 'bs-demo-frontend-script' );
-	wp_enqueue_style( 'bs-demo-frontend-styles' );
+	wp_enqueue_script( 'smplfy-demo-frontend-script' );
+	wp_enqueue_style( 'smplfy-demo-frontend-styles' );
 }
 
-add_action( 'wp_enqueue_scripts', 'bs_enqueue_boilerplate_frontend_scripts' );
+add_action( 'wp_enqueue_scripts', 'enqueue_boilerplate_frontend_scripts' );
